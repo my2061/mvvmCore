4bd48130617e3d91302155f61bebe4d80733d651
mengyang <2402334261@qq.com>
2022-07-19 18:05:45 +0800

    up

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,39 @@
 export default class Mue{
-    
-}
+    public vm: object = {};
 
-console.log("21309");
+    constructor(options: object){
+        this.init(options);
+    }
+
+    init(options: object){
+    }
+
+    __data(){
+
+    }
+
+    __methods(){
+
+    }
+
+    __watch(){
+
+    }
+
+    __computed(){
+
+    }
+    
+    /**
+     * 需要控制的容器
+     * @param {string} container
+     * @memberof Mue
+     */
+    public mount(container: string){
+        return this;
+    }
+
+    static createApp(counter: object){
+        
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 
-import Mue from "./core/index";
+// import Mue from "./core/index";
+
+// const options = {
+//     data(){
+//         return {
+//             hello: 1,
+//         }
+//     }
+// }
+
+// const app = new Mue(options).mount("#app");
 
 
-console.log("!23098");
+// console.log(app);
+// console.log(Vue);
+// console.log(Vue.createApp(options).mount("#app"));
+
+const vue = require("vue");
+
+console.log(vue)
